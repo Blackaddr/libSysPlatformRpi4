8d75eb8396d41382c567e9dbc030e7176816bc57
Blackaddr <slascos@gmail.com>
2024-01-29 13:51:31 -0500

    Move RPI4 run() and main() to SysInitialize.cpp

--- src/SysInitialize.cpp
+++ src/SysInitialize.cpp
@@ -1,11 +1,61 @@
+#include "sysPlatform/SysTypes.h"
+#include "sysPlatform/SysCpuControl.h"
+#include "sysPlatform/SysProgrammer.h"
+#include "sysPlatform/SysLogger.h"
+#include "sysPlatform/SysDebugPrint.h"
 #include "sysPlatform/SysInitialize.h"
 
 namespace SysPlatform {
 
-int  sysInitialize() { return 0; }
+static bool isInitialized = false;
 
-bool sysIsInitialized() { return false; }
+int  sysInitialize() {
+    isInitialized = true;
+    return SYS_SUCCESS;
+}
 
-void sysDeinitialize() { }
+bool sysIsInitialized() { return isInitialized; }
 
+void sysDeinitialize() {
+    isInitialized = false;
+    return;
+}
+
+}  // end namespace SysPlatform
+
+static SysPlatform::SysCpuControl::ShutdownMode run() {
+    using namespace SysPlatform;
+
+    //auto& codec = hardwareControls.getCodec();
+
+    while(true)
+	{
+        //if (sysProgrammer.isXferInProgress() && codec.isEnabled()) { codec.disable(); }
+
+        if (sysProgrammer.isNewProgrammingReceived()) {
+			SYS_DEBUG_PRINT(sysLogger.printf("***New firmware received, REBOOTING!!!"));
+			SysCpuControl::yield();  // must yield to give the TFTP thread a chance to close the connection
+			return SysCpuControl::ShutdownMode::REBOOT;
+		}
+
+        loop();
+    }
+}
+
+int main (void)
+{
+    using namespace SysPlatform;
+
+	if (!SysPlatform::sysInitialize()) {
+        return SysCpuControl::halt();
+    }
+    setup();
+	SysCpuControl::ShutdownMode mode = run();
+
+	switch (mode)
+	{
+	case SysCpuControl::ShutdownMode::REBOOT : return SysCpuControl::reboot ();
+	case SysCpuControl::ShutdownMode::HALT:
+	default: return SysCpuControl::halt ();
+	}
 }
