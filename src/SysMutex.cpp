8efa7494aad2ce3498c34939215aedc37abd599f
Steve Lascos <steve@blackaddr.com>
2024-10-27 10:35:28 -0400

    Add mutex::try_lock()

--- src/SysMutex.cpp
+++ src/SysMutex.cpp
@@ -9,6 +9,7 @@ namespace std {
 mutex::mutex()
 {
     mxPtr = new CMutex;
+    m_locked = false;
 }
 
 mutex::~mutex()
@@ -16,9 +17,22 @@ mutex::~mutex()
     if (mxPtr) { delete (CMutex*)mxPtr; }
 }
 
-void mutex::lock() { ((CMutex*)mxPtr)->Acquire(); }
+void mutex::lock() {
+    ((CMutex*)mxPtr)->Acquire();
+    m_locked = true;
+}
 //bool mutex<T>::try_lock() { return mx.try_lock(); }
-void mutex::unlock() { ((CMutex*)mxPtr)->Release(); }
+void mutex::unlock() {
+    m_locked = false;
+    ((CMutex*)mxPtr)->Release();
+}
+
+// TODO: Circle library doesn't support try_lock. Maybe we can try std::atmomic?
+bool mutex::try_lock() {
+    if (m_locked) { return false; }
+    lock();
+    return true;
+}
 
 }
 
